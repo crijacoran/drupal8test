bc57682df217981a7a8bd9a1577767e779d52dc8
Cristian Cogollo <cristian.cogollo@globant.com>
2019-05-28 15:45:02 -0500

    change executiotask

--- web/executiontask.sh
+++ web/executiontask.sh
@@ -4,3 +4,11 @@ cd /var/www/html/web
 chown -R www-data:www-data themes
 cd /var/www/html/web
 chown -R www-data:www-data modules
+cd /var/www/html
+composer install --no-dev --optimize-autoloader
+npm install
+gulp build:prod
+cd /var/www/html/web
+drush updb -y
+drush cim -y
+
